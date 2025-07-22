e0aa63df806871e8bb76008f678c43b2c44b3de8
explore-12 <darshanreddy2347@gmail.com>
2025-07-22 09:48:36 +0530

    Updated program

---
+++ LCM.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LCM {
+    public static void main(String[] args) {
+        int a,b,i,j;
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter the first number");
+        a=sc.nextInt();
+        
+        System.out.println("enter the second number");
+        b=sc.nextInt();
+
+       int  max=Math.max(a, b);
+    //    i=max+1;
+       while(true){
+        if(max%a==0 && max%b==0){
+            // System.out.println(max);
+            break;
+        }
+        max++;
+       }
+    System.out.println(max);
+
+    }
+}
---
+++ gcd_euclid_algorithm.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class gcd_euclid_algorithm {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        int maxx_diff;
+        System.out.println("enter the first number");
+        a=sc.nextInt();
+        System.out.println("enter the second number");
+        b=sc.nextInt();
+      int   m=Math.min(a,b);
+        while(m>=1){
+        if(a==b){
+            System.out.println(a);
+            break;
+        }
+
+        if(a>b){
+            maxx_diff=a-b;
+            a=maxx_diff;
+            m=a;
+
+        }
+        else{
+            maxx_diff=b-a;
+            b=maxx_diff;
+            m=b;
+        }
+        }
+
+System.out.println(m);
+    }
+}
